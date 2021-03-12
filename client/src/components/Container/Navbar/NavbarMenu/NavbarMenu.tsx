45fcd1d901fac3e4ceba97affab151ab9736379e
Akimov-Ihor <pandable1996@gmail.com>
2021-03-12 17:31:12 +0200

    [Add]NavbarMenu

--- client/src/components/Container/Navbar/NavbarMenu/NavbarMenu.tsx
+++ client/src/components/Container/Navbar/NavbarMenu/NavbarMenu.tsx
@@ -1,31 +1,56 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react';
-import React from 'react';
+import React, { useState } from 'react';
+
 import { NavLink } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
 
 export const NavbarMenu:React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [activeItem, setActiveItem] = useState('home');
   return (
     <div css={css`
-         width: 20%;
+         //width: 20%;
+         min-width: 208px;
     `}
     >
-      <nav className="navbar-nav ui vertical fluid tabular menu">
-        <ul>
-          <li>
-            <NavLink to="/home">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/trending">Trending</NavLink>
-          </li>
-          <li>
-            <NavLink to="/recommendation">Recommendation</NavLink>
-          </li>
-          <li>
-            <NavLink to="/watch:1">Video test</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <Menu pointing vertical>
+        <Menu.Item
+          icon="home"
+          as={NavLink}
+          name="Home"
+          to="/home"
+          active={activeItem === 'home'}
+          onClick={() => setActiveItem('home')}
+        />
+        <Menu.Item
+          icon="fire"
+          as={NavLink}
+          name="Trending"
+          to="/trending"
+          active={activeItem === 'trending'}
+          onClick={() => setActiveItem('trending')}
+        />
+        <Menu.Item
+          icon="eye"
+          as={NavLink}
+          name="Recommendation"
+          to="/recommendation"
+          active={activeItem === 'recommendation'}
+          onClick={() => setActiveItem('recommendation')}
+        />
+        <Menu.Item
+          icon="video"
+          as={NavLink}
+          name="Video"
+          to="/watch:1"
+          active={activeItem === 'video'}
+          onClick={() => setActiveItem('video')}
+        />
+
+      </Menu>
+
     </div>
   );
 };
